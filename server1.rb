0c95f4667d5a8911ccb5eb0e4234693bffb90c47
Ootani Kousei <takumikou@smkwlab.net>
2026-07-06 12:34:30 +0900

    レスポンスにHTTPヘッダを追加
    コンテンツをHTMLで返答()

--- server1.rb
+++ server1.rb
@@ -5,11 +5,20 @@ require 'socket'
 
 def server s
   cmd, path , ver = s.gets.split " "
+  # HTTP/1.1として正しく返答
+  # 1行目 HTTP/1.0 200 OK
+  # 2行目 Content-Type text/html
+  # 3行目 空行
+  # 4行目 コンテンツ
+  # 最後 ソケットをクローズ
   if path == "/"
+    s.puts "HTTP/1.0 200 OK\r\n"
+    s.puts "Content-Type: text/html\r\n"
+    s.puts "\r\n"
     pp "INDEX"
-    s.puts "index"
+    s.puts "<s>index</s>"
   else
-    pp "OTHER"
+    pp path
     s.puts "other"
   end
   s.close
